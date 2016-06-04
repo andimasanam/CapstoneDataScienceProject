bdec546cc38584e86cb4bbb1fb57422519fa6653
Andi M Masanam <andimarimuthu@gmail.com>
2016-06-04 22:16:48 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,19 +5,11 @@ library('shinyjs')
 ui = fluidPage(
   shinyjs::useShinyjs(),
   shinyjs::inlineCSS(list(.big = "font-size: 2em")),
-  div(id = "myapp",
-      h2("Coursera Data Science Capstone - Final Project Submission"),
-      checkboxInput("big", "Bigger text", FALSE),
-      textInput("name", "Text Input :", ""),
-      p('You can type up to 2 words in sequence with space as system will auto detect the word(s) and suggest you the best predictive next word in table format.'),
+  div(id = "app",
+      h2("Capstone - Final Project Submission"),
+      textInput("name", "Input :", ""),
+      p('Type up to 2 words in sequence with space as system will auto detect the word(s) and suggest you the best predictive next word in table format.'),
       a(id = "toggleAdvanced", "Show/hide advanced info", href = "#"),
-      shinyjs::hidden(
-        div(id = "advanced",
-            p('- Presentation:', HTML("<a href='https://beta.rstudioconnect.com/englianhu/Final-Project-Submission-Slides/'>Coursera Data Science Capstone - Final Project Submission (Slides)</a>")),
-            p('- Author Profile:', HTML("<a href='https://beta.rstudioconnect.com/englianhu/ryo-eng/'>®γσ, Eng Lian Hu</a>")),
-            p('- GitHub:', HTML("<a href='https://github.com/englianhu/Coursera-Data-Science-Capstone'>Source Code</a>"))
-        )
-      ),
       p("Timestamp: ",
         span(id = "time", date()),
         a(id = "update", "Update", href = "#")
