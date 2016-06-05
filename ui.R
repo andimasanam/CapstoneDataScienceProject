034877ca93e1dd17954b74fab3c6fb7859eb7fcc
Andi M Masanam <andimarimuthu@gmail.com>
2016-06-05 11:58:14 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,26 +1,45 @@
-library('shiny')
-library('shinyjs')
+library(shiny)
+library(wordcloud)
 
-# Define UI for application that draws a histogram
-ui = fluidPage(
-  shinyjs::useShinyjs(),
-  shinyjs::inlineCSS(list(.big = "font-size: 2em")),
-  div(id = "app",
-      h2("Capstone - Final Project Submission"),
-      textInput("name", "Input :", ""),
-      p('Type up to 2 words in sequence with space as system will auto detect the word(s) and suggest you the best predictive next word in table format.'),
-      p("Timestamp: ",
-        span(id = "time", date()),
-        a(id = "update", "Update", href = "#")
-      ),
-      actionButton("reset", "Reset form")
-  ),
-  mainPanel(
-    verbatimTextOutput('text1'),
-    verbatimTextOutput('text2'),
-    br(),
-    div(id = "bestMatch", p('The best match next predictive word is :', htmlOutput('text3'))),
-    hr(),
-    DT::dataTableOutput('table')
-  )
+shinyUI(fluidPage(
+        titlePanel(
+                h1("Coursera Capstone Next Word Prediction Project")
+        ),
+        fluidRow(
+                column(4,
+                       wellPanel(
+                                h5("Type a sentence fragment of any length 
+                                and the app will predict the next word in that fragment. The application  
+                                create a word cloud of the possible alternatives to that word, up to 
+                                a maximum selectable number."),
+                                h5("An example fragment has been provided for demonstration purposes."),
+                                h5("Please note that this app only supports inputs in English."),
+                                br(),
+                                textInput("entry",
+                                        "Input sentence fragment:", "This is a"
+                                         ),
+                                br(),
+                                sliderInput("numSuggestions", "Select max. suggestions for word cloud", 1, 50, value = 10),
+                                submitButton("Predict"),
+                                br()
+                       )
+                ),
+
+                column(4,
+                        h3('Results of prediction'),
+                        br(),
+                        h4('You entered the following text:'),
+                        wellPanel(h4(textOutput("inputted")), style = "color:purple"),
+                        br(),
+                        h4('The best prediction for the next word is:'),
+                        wellPanel(h4(textOutput("prediction1")), style = "color:brown")
+                ),
+  
+                column(4,
+                       h3('Word Cloud'),
+                       h4("Here's a simple wordcloud of predicted words:"),
+                       plotOutput("plot")
+                )
+        )
+        )
 )
